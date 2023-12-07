ffc220c8975203714f74312805ba00b4d898009b
pranav1483 <pranavp1483@gmail.com>
2023-12-08 04:30:55 +0530

    update backend url

--- frontend/src/Services/UserServices.js
+++ frontend/src/Services/UserServices.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 export const BACKEND_BASE_URL_2 = "https://pranav1483-intellitask.hf.space/api/"
 export const BACKEND_BASE_URL_1 = 'http://localhost:8000/api/'
-export const BACKEND_BASE_URL = 'https://intelli-task.onrender.com/api/'
+export const BACKEND_BASE_URL_3 = 'https://intelli-task.onrender.com/api/'
+export const BACKEND_BASE_URL = 'https://intelli-task-backend.vercel.app/api/'
 class UserService {
     getUser(user) {
         return axios.post(BACKEND_BASE_URL + 'user', user)
